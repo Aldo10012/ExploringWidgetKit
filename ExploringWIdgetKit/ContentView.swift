6cbfe04e0e4073e835285c97362628b5c60288a5
Alberto <aldo10012@gmail.com>
2021-05-27 10:03:46 -0700

    first commit

---
+++ ExploringWIdgetKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExploringWIdgetKit
+//
+//  Created by Alberto Dominguez on 5/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExploringWIdgetKit/ExploringWIdgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringWIdgetKitApp.swift
+//  ExploringWIdgetKit
+//
+//  Created by Alberto Dominguez on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ExploringWIdgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
